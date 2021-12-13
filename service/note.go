19619c047ec454c1328dba8308c72b0741806fe0
Osman Chowdhury <chowdhuryosman04@gmail.com>
2021-12-13 20:02:21 +0600

    #4: add flash command

--- service/note.go
+++ service/note.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -15,12 +16,29 @@ func (n *NoteService) CreateCommand(cmd *cobra.Command, args []string) error {
 	title, _ := cmd.Flags().GetString("title")
 	title = strings.Trim(title, " ")
 	if title == "" {
-		response("empty note not allowed", true)
+		response("empty note not allowed", true, false, true)
 		return nil
 	}
 
 	repo := repository.GetNewLocalStorage()
 	repo.AddNote(title)
+
+	return nil
+}
+
+func (n *NoteService) FlashStorageCommand(cmd *cobra.Command, args []string) error {
+	var confirm string
+	repo := repository.GetNewLocalStorage()
+
+	response("This will remove all preset note/config", false, true, true)
+	response("Are you sure you want to continue? [Y/n] ", false, true, false)
+	fmt.Scanf("%s", &confirm)
+	if !(confirm == "Y" || confirm == "n") {
+		response("Wrong input", true, false, true)
+	} else if confirm == "Y" {
+		repo.FlashStorage()
+		response("storage flashed", false, false, true)
+	}
 
 	return nil
 }
--- service/response.go
+++ service/response.go
@@ -2,10 +2,16 @@ package service
 
 import "fmt"
 
-func response(msg string, isError bool) {
+func response(msg string, isError, isWarning, newline bool) {
 	if isError {
-		fmt.Printf("yatt[error]: %s\n", msg)
+		fmt.Printf("=> yatt[error]: %s", msg)
+	} else if isWarning {
+		fmt.Printf("=> yatt[warning]: %s", msg)
 	} else {
-		fmt.Printf("yatt: %s\n", msg)
+		fmt.Printf("=> yatt: %s", msg)
+	}
+
+	if newline {
+		fmt.Print("\n")
 	}
 }
